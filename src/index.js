d2ea017802dfc6bf01ffc33486521759cdd9bc5f
Aditya Zingade <zingadeaditya24@gmail.com>
2020-11-10 10:05:38 +0530

    Create a React app and publish it to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,82 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonsDisplay from './SeasonsDisplay';
+import Spinner from './Spinner';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//Functional Component
+/*
+const App = () => {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    window.navigator.geolocation.getCurrentPosition(
+        position => console.log(position),
+        err => console.log(err)
+    );
+
+    return (
+        <div>
+            Hi There !!!
+        </div>
+    );
+};*/ 
+
+//Class Component
+
+class App extends React.Component{
+
+    /*
+    constructor(props){
+        super(props);
+
+        this.state = { lat: null, errMessage : "" };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ lat: position.coords.latitude});
+            },
+            err => {
+                this.setState({ errMessage : err.message })
+            }
+        );
+
+    }*/
+
+    state = { lat: null, errMessage: ""};
+
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ lat: position.coords.latitude});
+            },
+            err => {
+                this.setState({ errMessage : err.message })
+            }
+        );
+    }
+
+    render(){
+        
+    
+        if (this.state.lat && !this.state.errMessage){
+            return (
+                <SeasonsDisplay lat = { this.state.lat }>
+
+                </SeasonsDisplay>
+            );
+        }
+
+        if (!this.state.lat && this.state.errMessage){
+            return (
+                <div>
+                    Error: {this.state.errMessage}
+                </div>
+            );
+        }
+
+        return (
+            <Spinner/>
+        );
+        
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
