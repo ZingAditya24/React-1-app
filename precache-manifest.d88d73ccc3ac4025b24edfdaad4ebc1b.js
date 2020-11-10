699970ad819294c9712320d8d15c6268fc9c8245
Aditya Zingade <zingadeaditya24@gmail.com>
2020-11-10 10:01:00 +0530

    Updates

---
+++ precache-manifest.d88d73ccc3ac4025b24edfdaad4ebc1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b73aacd93610f2780c263b439e2b69e2",
+    "url": "/React-1-app/index.html"
+  },
+  {
+    "revision": "16ec85b0aae06c80b595",
+    "url": "/React-1-app/static/css/main.a26dd7ea.chunk.css"
+  },
+  {
+    "revision": "0c06334447f7cd91cf9b",
+    "url": "/React-1-app/static/js/2.5c6c98d1.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/React-1-app/static/js/2.5c6c98d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16ec85b0aae06c80b595",
+    "url": "/React-1-app/static/js/main.58592acb.chunk.js"
+  },
+  {
+    "revision": "3be63d0466b4e4eac0df",
+    "url": "/React-1-app/static/js/runtime-main.d4858379.js"
+  }
+]);
